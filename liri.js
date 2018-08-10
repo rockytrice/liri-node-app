a689adb42715bd6eb15692fdb5c78f3ca273148c
Roderick Trice <rocky_trice@yahoo.com>
2018-08-10 17:55:52 -0400

    keys file loaded to liri.js

--- liri.js
+++ liri.js
@@ -1,2 +1,26 @@
 require("dotenv").config();
 
+var keys = require('./keys.js');
+console.log(keys);
+
+
+
+var Twitter = require('twitter');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fs package to read/write
+var fs = require("fs");
+
+
