83df90f0055885854094ad4e8346b21094fe2fbe
Roderick Trice <rocky_trice@yahoo.com>
2018-08-10 20:34:21 -0400

    switch case for tweets

--- liri.js
+++ liri.js
@@ -1,11 +1,36 @@
+// read and set the env variables
 require("dotenv").config();
-
-var keys = require('./keys.js');
-console.log(keys);
-
-
-
+// import the request npm package
+// var request = require("request");
+// import the twitter npm package
 var Twitter = require('twitter');
+// import the spotify npm package
+var Spotify = require('node-spotify-api');
+// requiring the keys file
+var keys = require('./keys.js');
+
+// switch-case statement to direct which function gets run
+switch (action) {
+    case "my-tweets":
+      myTweets(functionData);
+      break;
+}
+
+// ES6 way of writing function to get the tweet data
+var myTweets = () => {
+    var client = new Twitter(keys.twitter);
+
+    var params = {screen_name: 'trice_rocky'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+      for(var i = 0; i < tweets.length; i++){
+
+        console.log(tweets[i].created_at);
+
+      }
+  }
+});
+}
 
 
 
@@ -17,10 +42,5 @@ var Twitter = require('twitter');
 
 
 
-
-
-
-// fs package to read/write
-var fs = require("fs");
 
 
